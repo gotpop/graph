bcc6b8a4776f299d9abfe9be6344b8339d3b96cb
gotpop <liam@gotpop.net>
2022-09-11 16:03:21 +0100

    Movies

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,17 +1,29 @@
-const { UserList } = require("../Fake-data");
+const { UserList, MovieList } = require("../Fake-data");
 const _ = require("lodash");
 
 const resolvers = {
     Query: {
+        // User resolvers
         users: () => {
             return UserList
         },
         user: (parent, args) => {
             const id = args.id
             const user = _.find(UserList, { id: Number(id) });
-
+            
             return user;
-        }
+        },
+        // Movie  resolvers
+        movies: () => {
+            return MovieList
+        },
+        movie: (parent, args) => {
+        console.log('args :', args);
+            const name = args.name
+            const movie = _.find(MovieList, { name });
+            
+            return movie;
+        },
     }
 
 }
