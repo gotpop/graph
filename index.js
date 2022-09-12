6bb1e8263a81a542552c760b465c027c3129f3b3
gotpop <liam@gotpop.net>
2022-09-12 16:33:12 +0100

    Build

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("./schema/type-defs");
-const { resolvers } = require("./schema/resolvers");
+const { ApolloServer } = require("apollo-server")
+const { typeDefs } = require("./schema/type-defs")
+const { resolvers } = require("./schema/resolvers")
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({url})=> {
-    console.log(`You API is running on: ${url}`);
-});
+    console.log(`You API is running on: ${url}`)
+})
